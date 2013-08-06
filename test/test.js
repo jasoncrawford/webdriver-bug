0c639043f642c5718defbf5b11dbef9a83c15087
Jason Crawford <jason@jasoncrawford.org>
2013-08-06 15:58:49 -0700

    Include the server in the repo

--- test/test.js
+++ test/test.js
@@ -3,24 +3,19 @@ var webdriver = require('selenium-webdriver');
 var remote = require('selenium-webdriver/remote');
 var test = require('selenium-webdriver/testing');
 
-var jarpath = process.env['SELENIUM'];
-if (!jarpath) {
-  process.stderr.writeln('Please set the environment variable SELENIUM to the path to your Selenium server standalone jar.')
-  process.exit(-1);
-}
+var jarpath = './selenium-server-standalone-2.34.0.jar';
+var url = 'http://medialize.github.io/jQuery-contextMenu/demo.html';
 
 var server = new remote.SeleniumServer({jar: jarpath});
 server.start();
 
 var builder = new webdriver.Builder().usingServer(server.address());
 
-var url = 'http://medialize.github.io/jQuery-contextMenu/demo.html';
-
 function testContextMenu(driver) {
   driver.get(url);
   var element = driver.findElement(webdriver.By.css('.context-menu-one'));
   driver.actions().click(element, webdriver.Button.RIGHT).perform();
-  return driver.findElement(webdriver.By.css('#context-menu-layer'));
+  driver.findElement(webdriver.By.css('#context-menu-layer'));
 }
 
 function testBrowser(browser) {
